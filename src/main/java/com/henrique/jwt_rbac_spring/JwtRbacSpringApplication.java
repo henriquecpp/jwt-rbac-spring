ab04163a354188ded2cc9851de1545fe02ec87d7
henriquecpp <henriquedotcpp@gmail.com>
2026-03-08 22:12:03 -0300

    feat: initial commit

---
+++ src/main/java/com/henrique/jwt_rbac_spring/JwtRbacSpringApplication.java
@@ -0,0 +1,13 @@
+package com.henrique.jwt_rbac_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRbacSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtRbacSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henrique/jwt_rbac_spring/JwtRbacSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henrique.jwt_rbac_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRbacSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
