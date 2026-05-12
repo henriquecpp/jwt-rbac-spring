a7f79d4e30a33fd045c5668e476d53ac0383714e
henriquecpp <henriquedotcpp@gmail.com>
2026-05-12 12:57:58 -0300

    feat: cria repositório UserJpaRepository para usuários
    - adiciona interface UserJpaRepository que estende JpaRepository
    - implementa métodos para buscar e verificar existência de usuários por email

---
+++ src/main/java/com/henrique/jwt_rbac_spring/infrastructure/persistence/repository/UserJpaRepository.java
@@ -0,0 +1,13 @@
+package com.henrique.jwt_rbac_spring.infrastructure.persistence.repository;
+
+import com.henrique.jwt_rbac_spring.infrastructure.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserJpaRepository extends JpaRepository<UserEntity, UUID> {
+
+    Optional<UserEntity> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
