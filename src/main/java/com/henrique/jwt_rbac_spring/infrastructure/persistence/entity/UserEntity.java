3256675484b8efbdb7164803cfd24e32842c9da2
henriquecpp <henriquedotcpp@gmail.com>
2026-05-12 12:57:48 -0300

    feat: cria entidade User para controle de usuários
    - adiciona a classe UserEntity como representação da entidade de usuário
    - inclui atributos como id, nome, email, senha e status da conta
    - implementa relacionamento muitos-para-muitos com a entidade Role

---
+++ src/main/java/com/henrique/jwt_rbac_spring/infrastructure/persistence/entity/UserEntity.java
@@ -0,0 +1,48 @@
+package com.henrique.jwt_rbac_spring.infrastructure.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder.Default
+    private boolean enabled = true;
+
+    @Builder.Default
+    private boolean accountNonLocked = true;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    @Builder.Default
+    private Set<RoleEntity> roles = new HashSet<>();
+}
